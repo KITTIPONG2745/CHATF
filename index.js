ec103a7734c921b4a1b5c260caff2e8f09694b63
KITTIPONG2745 <153720775+KITTIPONG2745@users.noreply.github.com>
2025-02-12 15:52:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ let app = express();
 
 let port = process.env.port || 3000;
 
-app.set('view engine', 'ejs');  // แก้ไข typo
+app.set('view engine', 'ejs');  // Fixed typo here
 
 app.get('/', (req, res) => {
     res.render('Home');
 });
 
-app.get('/ket', (req, res) => {
+app.get('/K', (req, res) => {
     res.render('Ket');
 });
 
-app.get('/non', (req, res) => {
+app.get('/N', (req, res) => {
     res.render('Non');
 });
 
-app.get('/te', (req, res) => {
-    res.render('Te');  // ตรวจสอบให้ตรงกับชื่อไฟล์ของเทมเพลต
+app.get('/HT', (req, res) => {
+    res.render('Hong Te');
 });
 
-app.get('/earth', (req, res) => {
+app.get('/ET', (req, res) => {
     res.render('Earth');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);  // แก้ไขการใช้ string interpolation
+    console.log(`Server is running on port ${port}`);  // Corrected string interpolation
 });
